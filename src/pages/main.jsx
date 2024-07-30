e163b8e4661eef97b4cb7f9ca5b12c969900a594
eunchae <yonj2sj@naver.com>
2024-07-30 16:10:06 +0900

    feat: 2차 합치기..

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -9,6 +9,10 @@ import food6 from "../assets/images/Food/Food6.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import beef from "../assets/images/beef.mp4";
+import believe from "../assets/images/believe.png";
+import comfortable from "../assets/images/comfortable.png";
+import BP from "../assets/images/BP.png";
 
 const main = () => {
   const settings = {
@@ -61,6 +65,19 @@ const main = () => {
   ];
   return (
     <div>
+      <video
+        src={beef}
+        autoPlay
+        loop
+        muted
+        type="/beef.mp4"
+        className="bfImg"
+      ></video>
+      <div className="content">
+        <h1 className="con1">숯불구이전문점</h1>
+        <p className="con2">마장동 고기집</p>
+      </div>
+
       <section className="main-Information">
         <div className="menu-name">마장동 고기집 메뉴안내</div>
         <div className="menu-Description">
@@ -91,9 +108,52 @@ const main = () => {
       <section className="main-store">
         <div>가맹정 현황</div>
       </section>
+
       <section className="main-system">
-        <div>마장동은 다릅니다.</div>
+        <div className="txt1">
+          <h1 className="txt2">표준화되고 안정적인 시스템</h1>
+          <p className="txt3">마장동은 다릅니다</p>
+        </div>
+
+        <iframe
+          className="conan"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/I4LVT9C6Sz4?si=r-aMJoSJtRfRxD54"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+        <div>
+          <img className="be" src={believe} />
+          <div className="box1">
+            <h1 className="be1">믿을 수 있는 품질</h1>
+            <p className="be2">마장동 고기집의 육가공 공장에서</p>
+            <p className="be3">최상위 품질의 고기를 선별하여 납품합니다</p>
+          </div>
+        </div>
+
+        <div>
+          <img className="comf" src={comfortable} />
+          <div className="com">
+            <h1 className="com1">운영의 편리함</h1>
+            <p className="com2">원팩으로 납품받아 고기를 손질하거나</p>
+            <p className="com3">썰지 않아도 운영이 가능합니다</p>
+          </div>
+        </div>
+
+        <div>
+          <img className="BBP" src={BP} />
+          <div className="BPP">
+            <h1 className="BPP1">비용절감</h1>
+            <p className="BPP2">손질이 완료된 원팩 납품으로 고기 담당자에</p>
+            <p className="BPP3">들어가는 인건비용 및 고기 로스가 없습니다</p>
+          </div>
+        </div>
       </section>
+
       <section className="main-money1">
         <div>수익안내</div>
       </section>
