1c323476406a7afea4964946b79b60f52a64ed06
kyuwan <gyuwann@gmail.com>
2024-07-30 20:56:30 +0900

    거의 완성본

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 import Instagram from "../../assets/images/Instagram.png";
 import Youtube from "../../assets/images/Youtube.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector(".Header-wrapper");
+      if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="Header-wrraper">
+    <div className="Header-wrapper">
       <div className="Header-left">
         <Link to="/">
           <div className="page-name">마 장 동</div>
@@ -19,17 +35,16 @@ const Header = () => {
         <span>프랜차이즈</span>
         <span>창업문의</span>
       </div>
-
       <div className="Header-right">
         <div className="Instagram">
           <img
-            classname="Instagram-icon"
+            className="Instagram-icon"
             src={Instagram}
             alt="인스타그램 아이콘"
           />
         </div>
         <div className="Youtube">
-          <img classname="Youtube-icon" src={Youtube} alt="유튜브 아이콘" />
+          <img className="Youtube-icon" src={Youtube} alt="유튜브 아이콘" />
         </div>
       </div>
     </div>
