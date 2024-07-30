c81bd634a58508fb95610261eba9dce61b6283a1
kyuwan <gyuwann@gmail.com>
2024-07-30 16:04:07 +0900

    서로 완성본 합치기

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -98,10 +98,190 @@ const main = () => {
         <div>수익안내</div>
       </section>
       <section className="main-startups">
-        <div>고기집은 역시, 마장동</div>
+        <div className="startups">
+          고기집은 역시, <div className="startups-name">마장동</div>
+        </div>
+        <div className="startups-information">
+          <p>창업에 꼭 필요한 비용만으로 구성하였습니다.</p>
+          <p>타 브랜드 대비 창업 비용이 약 20~30% 저렴합니다.</p>
+        </div>
+        <div className="startups-table">
+          <table>
+            <thead>
+              <tr>
+                <th>매장평수</th>
+                <th>25-30</th>
+                <th>35-40</th>
+                <th>45-50</th>
+                <th>55-60</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan={1}>
+                  가맹비
+                  <br />
+                  (상권보호, 상표사용, 지원납품)
+                </td>
+
+                <td colSpan={4}>1,000만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>
+                  본사이익금
+                  <br />
+                  (관리, 도면, 컨설팅, 교육, 오픈지원)
+                </td>
+
+                <td colSpan={4}>1,500만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>
+                  인테리어
+                  <br />
+                  (목공사, 바닥, 전기노출, 조명, 도색, 주방건식)
+                </td>
+                <td>3,000만원</td>
+                <td>3,500만원</td>
+                <td>4,000만원</td>
+                <td>4,500만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>주방집기비품, 숯기계</td>
+                <td>2,400만원</td>
+                <td>2,600만원</td>
+                <td>2,800만원</td>
+                <td>3,000만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>
+                  닥터공사
+                  <br />
+                  (임상별도)
+                </td>
+                <td>700만원</td>
+                <td>800만원</td>
+                <td>900만원</td>
+                <td>1,000만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>
+                  전면간판, 메뉴판, 액자, 현수막, 실사, 사인물전체
+                  <br />
+                  (투광기, 돌출간판 별도)
+                </td>
+                <td>700만원</td>
+                <td>800만원</td>
+                <td>900만원</td>
+                <td>1,000만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>수저통일체형 테이블, 의자</td>
+                <td>400만원</td>
+                <td>500만원</td>
+                <td>600만원</td>
+                <td>700만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>40평 스탠드형 냉난방기, 추가배관별도</td>
+                <td>600만원</td>
+                <td>900만원</td>
+                <td>1,200만원</td>
+                <td>1,500만원</td>
+              </tr>
+              <tr>
+                <td colSpan={1}>
+                  별도(철거, 소방, 도시가스, 전기승압, 닥터임상,
+                  오픈광고(온/오프라인),
+                  <br /> 유니폼, 명함, 전단지, 계열각현수막, 입주청소, 샤시,
+                  아웃테리어, 화장실)
+                </td>
+                <td colSpan={4}>평균 800~1500만원(별도발생)</td>
+              </tr>
+              <tr>
+                <td className="td-td">합계</td>
+                <td className="td-td2">10,300</td>
+                <td className="td-td2">11,600</td>
+                <td className="td-td2">12,900</td>
+                <td className="td-td2">13,700</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="startups-discount">
+          <div className="discount-text1">업종 변경시</div>
+          <div className="discount-text2">
+            더욱 <div className="discount">저렴</div>합니다!
+          </div>
+        </div>
       </section>
       <section className="Inquiry">
-        <div>창업 문의</div>
+        <div className="Inquiry-text">창업 문의</div>
+        <div className="Inquiry-text2">
+          <p>창업문의를 남겨주시면</p>
+          <p>최대한 빠른 시일 내에 연락 드리겠습니다.</p>
+        </div>
+        <div className="form-containor">
+          <div className="Inquiry-form">
+            <div className="form-name">
+              <div className="form-text">
+                <span>성함 *</span>
+              </div>
+              <input type="text" placeholder="이름을 작성해주세요."></input>
+            </div>
+            <div className="form-tel">
+              <div className="form-text">
+                <span>연락처 *</span>
+              </div>
+              <div className="tel-form">
+                <input type="tel"></input>
+                <div className="var">
+                  <span>-</span>
+                </div>
+                <input type="tel"></input>
+                <div className="var">
+                  <span>-</span>
+                </div>
+                <input type="tel"></input>
+              </div>
+            </div>
+            <div className="form-gps">
+              <div className="form-text">
+                <span>창업희망지역 *</span>
+              </div>
+              <input
+                type="text"
+                placeholder="창업희망지역을 입력해주세요."
+              ></input>
+            </div>
+            <div className="form-inquiry">
+              <div className="form-text">
+                <span>문의사항 *</span>
+              </div>
+              <input type="text" placeholder="문의사항을 입력해주세요."></input>
+            </div>
+            <div className="form-choice">
+              <div className="form-text">
+                <span>창업종류 *</span>
+              </div>
+              <input type="radio" id="new-business" name="business-type" />
+              <label htmlFor="new-business">신규창업</label>
+              <input type="radio" id="change-industry" name="business-type" />
+              <label htmlFor="change-industry">업종변경</label>
+            </div>
+            <div className="form-checkbox">
+              <input
+                type="checkbox"
+                id="Personal-information"
+                name="information-type"
+              />
+              <span>개인정보 수집 및 이용에 동의합니다.</span>
+            </div>
+            <div className="form-button">
+              <button>문의하기</button>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
