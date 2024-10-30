f136631bcfa2447fcc483bc24825842bab9f942f
kyuwan <gyuwann@gmail.com>
2024-10-30 16:57:31 +0900

    feat: 마장규 완성에 가까운

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -118,12 +118,14 @@ const main = () => {
             <iframe
               className="youtubelink"
               width="100%"
-              height="400"
-              src="https://www.youtube.com/embed/Jl-_LVLGsNI"
+              height="40%"
+              src="https://www.youtube.com/embed/Jl-_LVLGsNI?start=1"
               title="YouTube video player"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
+
             <div className="be-box">
               <img className="comf" src={believe} alt="believe" />
               <div className="com">
