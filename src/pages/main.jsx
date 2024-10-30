6bc50061574cb2072a19c8590a26b23d8f60aed7
eunchae <yonj2sj@naver.com>
2024-10-30 15:30:29 +0900

    feat: 최종입니다

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -116,15 +116,13 @@ const main = () => {
         <div className="system-page">
           <div className="system-left">
             <iframe
-              className="conan"
+              className="youtubelink"
               width="100%"
               height="400"
-              src="https://www.youtube.com/embed/I4LVT9C6Sz4?si=r-aMJoSJtRfRxD54"
+              src="https://www.youtube.com/embed/Jl-_LVLGsNI"
               title="YouTube video player"
-              frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
             <div className="be-box">
               <img className="comf" src={believe} alt="believe" />
